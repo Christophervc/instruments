74f4bbe22dc111e4de25e0d23ed86c68f47469ba
Oscar Vasquez <74890059+Christophervc@users.noreply.github.com>
2025-08-23 23:15:46 -0500

    Feat: sorting product by name ascending

--- instruments/src/main/java/com/aplication/rest/instruments/repository/ManufacturerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.aplication.rest.instruments.repository;
-
-import com.aplication.rest.instruments.entities.Manufacturer;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ManufacturerRepository extends CrudRepository<Manufacturer, Long> {
-}
